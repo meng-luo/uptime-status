4a52c8c7fd1066d591854cf781a4787093d9bc23
meng-luo <guorongdan1205@163.com>
2022-08-06 22:04:05 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '梦璃雨落の状态页',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1383340-e7b0c45bd38d39cadb0b1622',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '主页',
+      url: 'https://home.dreamfall.cn/'
+    },
+    {
+      text: '博客',
+      url: 'https://blog.dreamfall.cn'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/meng-luo/'
+    },
+  ],
+};
